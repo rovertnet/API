4e21289401234518bf442ccd051ee80f757a48e7
Robert Matundu <matundukabamba@gmail.com>
2024-03-17 15:37:43 +0100

    je suis un autre commit

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Route;
 //Qui concerne l'API
 Route::get('/posts', [postCrud_ctrl::class, 'index']);
 Route::post('/posts/create', [postCrud_ctrl::class, 'store']);
+Route::post('/posts/edit(id)', [postCrud_ctrl::class, 'update']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
