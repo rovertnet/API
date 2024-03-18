fa7ea91c7274b1c28609a525b6745c548b9d96e1
Robert Matundu <matundukabamba@gmail.com>
2024-03-18 16:50:46 +0100

    je suis un autre commit

--- app/Http/Requests/authUser.php
+++ app/Http/Requests/authUser.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class authUser extends FormRequest
 {
@@ -22,7 +24,30 @@ class authUser extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required',
+            'email' => 'required|unique:users,email',
+            'password' => 'required'
+        ];
+    }
+    
+    public function failedValidation(Validator $validator){
+        
+        throw new HttpResponseException(response()->json([
+            'SUCCESS' => false,
+            'status_code' => 422,
+            'error' => true,
+            'message' => 'erreur de validation',
+            'errorList' => $validator->errors()
+        ]));
+    }
+
+    //Message de la condition posée
+    public function messages(){
+        return[
+          'name.required' => 'Le nom doit être fourni',
+          'email.required' => 'Email doit être fourni',
+          'email.unique' => 'Email existe déjà',
+          'password.required' => 'Le mot de passe doit être fourni'
         ];
     }
 }
