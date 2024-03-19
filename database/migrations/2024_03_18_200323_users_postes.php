1cc97e3716192ed4456212adcd71c9dfa49027d4
Robert Matundu <matundukabamba@gmail.com>
2024-03-19 18:50:32 +0100

    je suis un autre commit

--- database/migrations/2024_03_18_200323_users_postes.php
+++ database/migrations/2024_03_18_200323_users_postes.php
@@ -11,7 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::table('posts', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+        });    
     }
 
     /**
@@ -19,6 +22,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropColumn('user_id');
+        }); 
     }
 };
